1df1f789b7c7a54512c65115b3867e6d2772837f
Pratik <pratik16082001@gmail.com>
2024-01-23 16:25:48 +0530

    integrated leaflet

---
+++ healthify-me-next/components/map.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import React from "react";
+import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+
+type mapProps = {
+  userLocation: {
+    lat: number;
+    lng: number;
+    zoomLevel: number;
+  };
+  onMapClick: (latlng: { lat: number; lng: number }) => void;
+};
+
+const MapClickHandler = ({ onMapClick }: any) => {
+  useMapEvents({
+      click: (e) => {
+          onMapClick(e.latlng);
+      },
+  });
+  return null; 
+  // this component does not render anything
+};
+
+export default function LeafletMap({ userLocation, onMapClick }: mapProps) {
+  const { lat, lng, zoomLevel } = userLocation;
+
+  return (
+    <MapContainer style={{ height: "100%", width: "100%" }} center={[lat, lng]} zoom={zoomLevel} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MapClickHandler onMapClick={onMapClick} />
+    </MapContainer>
+  );
+}
